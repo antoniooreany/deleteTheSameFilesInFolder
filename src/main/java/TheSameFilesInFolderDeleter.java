cd821ccd65e7810dd117aa849b2e40c000c1ac10
Anton Gorshkov <antonio_oreany@mail.ru>
2018-05-31 09:38:00 +0300

    Renaming unnecessary files function added before deleting them. This function has been commented.

--- src/main/java/TheSameFilesInFolderDeleter.java
+++ src/main/java/TheSameFilesInFolderDeleter.java
@@ -17,14 +17,16 @@ public class TheSameFilesInFolderDeleter {
             }
             int equalLengthCount = 0;
             for (File entry1 : folderEntries) {
-//                System.out.printf("entry0.length() = %d%n", length0);
-//                System.out.printf("entry1.length() = %d%n", length1);
                 final long length0 = entry0.length();
                 final long length1 = entry1.length();
                 if (length0 == length1) {
                     equalLengthCount++;
                 }
                 if (equalLengthCount > 1) {
+//                    final String entry1Name = entry1.getName();
+//                    final String entry1Parent = entry1.getParent();
+//                    final File entry1Dest = new File(entry1Parent + "\\____" + entry1Name);
+//                    final boolean renameTo = entry1.renameTo(entry1Dest);
                     final boolean deleted = entry1.delete();
                     equalLengthCount = 0;
                 }
